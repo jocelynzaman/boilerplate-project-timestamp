d78fee935a7594dc074b08d56ca5ca2252f0ec2f
Jocelyn Zaman <jzaman@asu.edu>
2024-10-10 12:21:41 -0700

    added date request

--- index.js
+++ index.js
@@ -24,7 +24,26 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//date request
+app.use("/api/:date", function (req, res) {
+  var date = req.params.date;
+  date = (req.params.date.includes("-")) ? new Date(req.params.date) : new Date(Number(req.params.date));
+  var time = date.getTime();
+  if (!isNaN(time))
+  {
+    res.json({"unix": time, "utc": date.toUTCString()});
+  }
+  else
+  {
+    res.json({ error : "Invalid Date" });
+  }
+});
 
+ //empty date request
+app.use("/api/", function (req, res) {
+  var date = Date.now();
+  res.json({"unix": date, "utc": new Date(Number(date)).toUTCString()});
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
